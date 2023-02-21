16f4cfe12328f87fb03f33e61e08def1ffb604a1
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2023-02-21 10:06:08 +0200

    innit: start write code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.min = 0;
+    this.max = 0;
+    }
 
     setRange(min, max) {
-
+    this.min=min;
+    this.max=max;      
     }
 
     guess() {
-
+    return Math.round((this.min + this.max) / 2)
     }
 
     lower() {
-
+     this.min=this.guess();
     }
 
     greater() {
-
+    this.max=this.guess();
     }
 }
 
